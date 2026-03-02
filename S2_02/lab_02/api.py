5abe8723b4311e59899ac2035f48c30ea23998c8
Caothuuyenit <Caothuuyen.it@gmail.com>
2026-03-02 11:00:03 +0700

    vigenere

--- S2_02/lab_02/api.py
+++ S2_02/lab_02/api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
+from cipher.vigenere.vigenere_cipher import VigenereCipher
 
 app = Flask(__name__)
 caesar_cipher = CaesarCipher()
@@ -37,6 +38,41 @@ def caeser_decrypt():
     decrypt_text = caesar_cipher.decrypt_text(cipher_text, int(key))
     return jsonify({'decrypt_message': decrypt_text})
 
+Vigenere_cipher = VigenereCipher()
 
+@app.route('/api/vigenere/encrypt', methods=['POST'])
+def vigenere_encrypt():
+    data = request.json
+    print("Received:", data)
+
+    if not data:
+        return jsonify({"error": "Missing JSON body"}), 400
+
+    plain_text = data.get('plain_text')
+    key = data.get('key')
+
+    if not plain_text or not key:
+        return jsonify({"error": "Missing plain_text or key"}), 400
+
+    encrypt_text = Vigenere_cipher.encrypt_text(plain_text, key)
+    return jsonify({'encrypt_message': encrypt_text})
+
+
+@app.route('/api/vigenere/decrypt', methods=['POST'])
+def vigenere_decrypt():
+    data = request.json
+    print("Received:", data)
+
+    if not data:
+        return jsonify({"error": "Missing JSON body"}), 400
+
+    cipher_text = data.get('cipher_text')
+    key = data.get('key')
+
+    if not cipher_text or not key:
+        return jsonify({"error": "Missing cipher_text or key"}), 400
+
+    decrypt_text = Vigenere_cipher.decrypt_text(cipher_text, key)
+    return jsonify({'decrypt_message': decrypt_text})
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ S2_02/lab_02/cipher/vigenere/vigenere_cipher.py
@@ -0,0 +1,43 @@
+class VigenereCipher:
+    def __init__(self):
+        pass
+
+    def encrypt_text(self, plain_text, key):
+        encrypt_text = ""
+        key = key.upper()
+        key_index = 0
+
+        for char in plain_text:
+            if char.isalpha():
+                key_shift = ord(key[key_index % len(key)]) - ord('A')
+
+                if char.isupper():
+                    encrypt_text += chr((ord(char) - ord('A') + key_shift) % 26 + ord('A'))
+                else:
+                    encrypt_text += chr((ord(char) - ord('a') + key_shift) % 26 + ord('a'))
+
+                key_index += 1
+            else:
+                encrypt_text += char
+
+        return encrypt_text
+
+    def decrypt_text(self, cipher_text, key):
+        decrypt_text = ""
+        key = key.upper()
+        key_index = 0
+
+        for char in cipher_text:
+            if char.isalpha():
+                key_shift = ord(key[key_index % len(key)]) - ord('A')
+
+                if char.isupper():
+                    decrypt_text += chr((ord(char) - ord('A') - key_shift) % 26 + ord('A'))
+                else:
+                    decrypt_text += chr((ord(char) - ord('a') - key_shift) % 26 + ord('a'))
+
+                key_index += 1
+            else:
+                decrypt_text += char
+
+        return decrypt_text
